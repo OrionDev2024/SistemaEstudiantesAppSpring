f54afd2609d32a289488b23b0e7df47ae5ccab53
William Lopez R <lopezrebellonw@gmail.com>
2024-12-10 23:33:31 -0500

    configuracion de run main

--- src/main/java/od/estudiantes/EstudiantesApplication.java
+++ src/main/java/od/estudiantes/EstudiantesApplication.java
@@ -1,13 +1,33 @@
 package od.estudiantes;
 
+
+import od.estudiantes.servicio.EstudianteServicio;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
 
 @SpringBootApplication
-public class EstudiantesApplication {
+public class EstudiantesApplication implements CommandLineRunner {
+
+	@Autowired
+	private EstudianteServicio estudianteServicio;
+
+	private static final Logger logger = LoggerFactory.getLogger(EstudiantesApplication.class);
+
+	String nl = System.lineSeparator();
 
 	public static void main(String[] args) {
+		logger.info("Iniciando la aplicación...");
+		//Levantar la fabrica de Spring
 		SpringApplication.run(EstudiantesApplication.class, args);
+		logger.info(("Aplicación finalizada!"));
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		logger.info("Ejecutando run de Spring...");
+	}
 }
