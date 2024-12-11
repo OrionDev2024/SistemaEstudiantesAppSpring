4385b0e49f1e03cde18c1275683490be46a8352f
William Lopez R <lopezrebellonw@gmail.com>
2024-12-10 22:45:13 -0500

    Modelo Estudiante con Lombok

---
+++ src/main/java/od/estudiantes/modelo/Estudiante.java
@@ -0,0 +1,26 @@
+package od.estudiantes.modelo;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Entity
+//boilerplate - codigo repetitivo
+@Data//getters y setters
+@NoArgsConstructor //constructor vacío
+@AllArgsConstructor //constructor con parametros
+@ToString //metodo ToString
+public class Estudiante {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int idEstudiante;
+    private String nombre;
+    private String apellido;
+    private  String telefono;
+    private String email;
+}
