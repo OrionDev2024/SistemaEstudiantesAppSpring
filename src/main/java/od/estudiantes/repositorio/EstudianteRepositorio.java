a987e1418de78b2eb98d2dee8ed433396df86de6
William Lopez R <lopezrebellonw@gmail.com>
2024-12-10 22:50:49 -0500

    creación interface Estudiante- extiende de clase Repositorio

---
+++ src/main/java/od/estudiantes/repositorio/EstudianteRepositorio.java
@@ -0,0 +1,7 @@
+package od.estudiantes.repositorio;
+
+import od.estudiantes.modelo.Estudiante;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EstudianteRepositorio extends JpaRepository<Estudiante, Integer> {
+}
